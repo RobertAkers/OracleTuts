dc6273d1d2c47bca1134fee30ce8f2a79bf836d1
Robert Akers <akersrobb@outlook.com>
2015-10-29 05:10:27 -0500

    Objects in Java, Tutorial One of Oracle's Java tutorial.

--- OracleTuts/src/OracleTutorials/Objects.java
+++ OracleTuts/src/OracleTutorials/Objects.java
@@ -3,7 +3,15 @@ package OracleTutorials;
 public class Objects {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*Objects in Java, like objects in the real world, have states in behaviors. When you think of a state, for example, you can think of
+		your dog. Your dog can be small, or large, or medium sized. It can be awake or sleeping, or it can be hungry or not hungry. In java 
+		these states are called variables. To stay with the dog example, your animal can also have behaviors - it can be eating or not eating,
+		barking or not barking, sleeping or awake. In java these are called methods. 
+		
+		Methods in java act on an objects state and change them. When your variables are 'private' and can only be accessed through methods,
+		this adds security to your program. Accessing and changing a java programs states through use of methods is called encapsulation,
+		and it is one of the most important concepts in the java language.*/
 
 	}
 
