5691712478cfc4b27857fc9969081e6d5b5f3a47
Robert Akers <akersrobb@outlook.com>
2015-10-30 00:09:42 -0500

    Schedule for tomorrow.

---
+++ OracleTuts/src/OracleTutorials/WhatIsAnInterface.java
@@ -0,0 +1,13 @@
+package OracleTutorials;
+
+public class WhatIsAnInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		//Schedule for tomorrow:
+			//Reread all previous tutorial. Complete What is an Interface - Variables (Questions and answers)
+	}
+
+}
